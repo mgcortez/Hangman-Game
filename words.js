d199ff07f1cc76b545ecefbe3a33c04cfc971f36
mgcortez <mgabrielacortez@outlook.com>
2017-02-17 23:16:14 -0500

    Add files via upload

---
+++ words.js
@@ -0,0 +1,17 @@
+var words = [{
+
+	word: "Michael Jordan",
+	blurb: "American Basketball player, GOAT, Played for Chicago Bulls",
+	imgLink: "assets/images/jumpman.png"
+},
+	word: "Spice Girls",
+	blurb: "English pop group, Global Phenomenon, Girl Power",
+	imgLink:"assets/images/spice.jpeg"
+},
+
+	word: "Power Rangers",
+	blurb: "Japanese series dubbed for American Audiences, Mighty Morphin..., Villian Rita Repulsa",
+	imgLink:"assets/images/power.jpeg"
+},
+	
+}]
